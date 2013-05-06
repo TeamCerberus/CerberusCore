19d3704a38616099c7ba25a393392eaa832028d9
Cayde Dixon <cayde.dixon@gmail.com>
2013-05-06 21:02:06 +1000

    Added @ConfigurationClass and more work on the networking (design incomplete)

--- src_common/teamcerberus/cerberuscore/config/ConfigurationParser.java
+++ src_common/teamcerberus/cerberuscore/config/ConfigurationParser.java
@@ -24,53 +24,20 @@ public class ConfigurationParser {
 		Field[] fields = clazz.getDeclaredFields();
 
 		for (Field field : fields) {
+			if (!field.isAccessible()) field.setAccessible(true);
+			
 			ConfigurationOption annotation = field
 					.getAnnotation(ConfigurationOption.class);
 			if (annotation == null) {
 				continue;
 			}
-			Class type = field.getType();
+
+			String category = annotation.category();
+			String comment = annotation.comment();
+			String key = annotation.key();
 
 			try {
-				if (type == boolean.class || type == Boolean.class) {
-					Boolean def = field.getBoolean(instance);
-					Boolean value = config.get(annotation.category(),
-							annotation.key(), def, annotation.comment())
-							.getBoolean(def);
-
-					field.setBoolean(instance, value);
-				} else if (type == double.class) {
-					Double def = field.getDouble(instance);
-					Double value = config.get(annotation.category(),
-							annotation.key(), def, annotation.comment())
-							.getDouble(def);
-
-					field.setDouble(instance, value);
-				} else if (type == String.class) {
-					String def = (String) field.get(instance);
-					String value = config.get(annotation.category(),
-							annotation.key(), def, annotation.comment())
-							.getString();
-
-					field.set(instance, value);
-				} else if (type == Integer.class || type == int.class) {
-					Integer def = field.getInt(instance);
-					Integer value = config.get(annotation.category(),
-							annotation.key(), def, annotation.comment())
-							.getInt(def);
-
-					field.setInt(instance, value);
-				} else if (type == Property.class) {
-					// Property def = (Property)field.get(instance);
-					Property value = config.get(annotation.category(),
-							annotation.key(), 0, annotation.comment());
-
-					field.set(instance, value);
-				} else {
-					CerberusLogger.logWarning("Type \"" + type.getName()
-							+ "\" is not supportd with annotations");
-				}
-
+				parseField(field, instance, category, key, comment, config);
 			} catch (IllegalArgumentException e) {
 				e.printStackTrace();
 			} catch (IllegalAccessException e) {
@@ -83,4 +50,94 @@ public class ConfigurationParser {
 		}
 	}
 
+	private static void parseField(Field field, Object instance,
+			String category, String key, String comment, Configuration config)
+			throws IllegalArgumentException, IllegalAccessException {
+		Class type = field.getType();
+
+		if (type == boolean.class || type == Boolean.class) {
+			Object def = field.get(instance);
+
+			Object value;
+			if (!comment.isEmpty()) value = config.get(category, key,
+					(Boolean) def, comment).getBoolean((Boolean) def);
+			else value = config.get(category, key, (Boolean) def).getBoolean(
+					(Boolean) def);
+
+			field.set(instance, value);
+		} else if (type == double.class || type == Double.class) {
+			Object def = field.get(instance);
+
+			Object value;
+			if (!comment.isEmpty()) value = config.get(category, key,
+					(Double) def, comment).getDouble((Double) def);
+			else value = config.get(category, key, (Double) def).getDouble(
+					(Double) def);
+
+			field.set(instance, value);
+		} else if (type == String.class) {
+			Object def = field.get(instance);
+
+			Object value;
+			if (!comment.isEmpty()) value = config.get(category, key,
+					(String) def, comment).getString();
+			else value = config.get(category, key, (String) def).getString();
+
+			field.set(instance, value);
+		} else if (type == Integer.class || type == int.class) {
+			Object def = field.get(instance);
+
+			Object value;
+			if (!comment.isEmpty()) value = config.get(category, key,
+					(Integer) def, comment).getInt((Integer) def);
+			else value = config.get(category, key, (Integer) def).getInt(
+					(Integer) def);
+
+			field.set(instance, value);
+		} else if (type == float.class) {
+			double def = field.getFloat(instance);
+			double value;
+			if (!comment.isEmpty()) value = config.get(category, key, def,
+					comment).getDouble(def);
+			else value = config.get(category, key, def).getDouble(def);
+
+			float actualValue = Float.valueOf(String.valueOf(value));
+			field.setFloat(instance, actualValue);
+
+		} else {
+			CerberusLogger.logWarning("Type \"" + type.getName()
+					+ "\" is not supportd with annotations");
+		}
+	}
+
+	public static void ParseClass(Object instance, Configuration config) {
+		Class clazz = instance.getClass();
+		
+		ConfigurationClass annotation = (ConfigurationClass) clazz
+				.getAnnotation(ConfigurationClass.class);
+		if (annotation == null) {
+			return;
+		}
+		String category = annotation.category();
+		Field[] fields = clazz.getDeclaredFields();
+		
+		for (Field field : fields) {
+			if (!field.isAccessible()) field.setAccessible(true);
+			ConfigurationComment commentAnnotation = field.getAnnotation(ConfigurationComment.class);
+			String comment = (commentAnnotation == null) ?  "" : commentAnnotation.comment();
+			String key = field.getName();
+			
+			try {
+				parseField(field, instance, category, key, comment, config);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+
+			if (config.hasChanged()) {
+				config.save();
+			}
+		}
+	}
 }
