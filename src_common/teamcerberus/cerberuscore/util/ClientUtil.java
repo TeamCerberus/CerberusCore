95216df8bdd76bf78bffe32ff6b4ee42b02bdb10
Cayde Dixon <cayde.dixon@gmail.com>
2013-05-12 19:21:31 +1000

    upgraded the XML parsing and cape and skin parsing

--- src_common/teamcerberus/cerberuscore/util/ClientUtil.java
+++ src_common/teamcerberus/cerberuscore/util/ClientUtil.java
@@ -1,6 +1,17 @@
 package teamcerberus.cerberuscore.util;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import teamcerberus.cerberuscore.xml.CerberusXMLParser;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 
@@ -16,10 +27,29 @@ public class ClientUtil {
 	public static void init() {
 		capes = new HashMap<String, String>();
 		skins = new HashMap<String, String>();
-		setCape("fzerox", "https://raw.github.com/TeamCerberus/TeamCapes/master/teamDefault.png");
-		setCape("cazzar", "https://raw.github.com/TeamCerberus/TeamCapes/master/cazzar.png");
-		setCape("Aroma1997", "https://raw.github.com/TeamCerberus/TeamCapes/master/Aroma1997.png");
-		setCape("DreadMagusX1","https://raw.github.com/TeamCerberus/TeamCapes/master/DreadMagusX1.png");
+		
+		try {
+			CerberusXMLParser xml = new CerberusXMLParser(new URL("https://raw.github.com/TeamCerberus/TeamCapes/master/info.xml").openStream());
+			for (Entry<String, Node> entry : xml.getAll().entrySet()) {
+				//System.out.println(entry.getKey() + ":" + xml.getNodeValue(entry.getValue()));
+				if (entry.getKey().split("\\.").length != 2)
+					continue;
+				
+				String username = entry.getKey().split("\\.")[0];
+				String part = entry.getKey().split("\\.")[1];
+				if (part.equalsIgnoreCase("cape") && !entry.getValue().getTextContent().isEmpty())
+					setCape(username, entry.getValue().getTextContent());
+				else if (part.equalsIgnoreCase("skin") && !entry.getValue().getTextContent().isEmpty())
+					setSkin(username, entry.getValue().getTextContent());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//setCape("fzerox", "https://raw.github.com/TeamCerberus/TeamCapes/master/teamDefault.png");
+		//setCape("cazzar", "https://raw.github.com/TeamCerberus/TeamCapes/master/cazzar.png");
+		//setCape("Aroma1997", "https://raw.github.com/TeamCerberus/TeamCapes/master/Aroma1997.png");
+		//setCape("DreadMagusX1","https://raw.github.com/TeamCerberus/TeamCapes/master/DreadMagusX1.png");
 	}
 
 	@Deprecated
--- src_common/teamcerberus/cerberuscore/xml/CerberusXMLParser.java
+++ src_common/teamcerberus/cerberuscore/xml/CerberusXMLParser.java
@@ -1,38 +1,61 @@
 package teamcerberus.cerberuscore.xml;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.XMLEvent;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 
 public class CerberusXMLParser {
-	public HashMap<String, String>				xmlData;
-	public HashMap<String, Iterator<Attribute>>	xmlAttributes;
+	HashMap<String, Node>	xmlData;
 
-	public CerberusXMLParser(InputStream XMLFile, Iterable<String> properties)
-			throws XMLStreamException {
-		xmlData = new HashMap<String, String>();
-		parse(XMLFile, properties);
+	public CerberusXMLParser(InputStream XMLFile) throws SAXException,
+			IOException, ParserConfigurationException {
+		xmlData = new HashMap<String, Node>();
+		final DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+        final Document doc = docBuilder.parse(XMLFile);
+        xmlData = new HashMap<String, Node>();
+        parse("", doc.getDocumentElement());
+    }
+
+    private void parse(String prefix, final Element e) {
+        final NodeList children = e.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            final Node n = children.item(i);
+            if (n.getNodeType() == Node.ELEMENT_NODE) {
+            	xmlData.put(prefix + n.getNodeName(), n);
+                parse(prefix + n.getNodeName() + ".", (Element) n);
+            }
+        }
+    }
+
+	public boolean containsKey(String key) {
+		return xmlData.containsKey(key);
 	}
 
-	private void parse(InputStream XMLFile, Iterable<String> properties)
-			throws XMLStreamException {
-		XMLInputFactory factory = XMLInputFactory.newInstance();
-		XMLEventReader reader = factory.createXMLEventReader(XMLFile);
+	public boolean containsValue(String value) {
+		return xmlData.containsKey(value);
+	}
 
-		while (reader.hasNext()) {
-			XMLEvent event = reader.nextEvent();
-			if (event.isStartElement()) {
-				String name = event.asStartElement().getName().getLocalPart();
-				//xmlAttributes.put(name, event.asStartElement().getAttributes());
-				xmlData.put(name, event.asCharacters().getData());
-			}
-		}
+	public Node get(String key) {
+		return xmlData.get(key);
+	}
+	
+	public HashMap<String, Node> getAll() {
+		return xmlData;
 	}
 }
